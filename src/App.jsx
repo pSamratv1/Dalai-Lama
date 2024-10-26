12af152380f921a1a046d202b08d932ce130c0c1
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-26 23:32:07 +0545

    Featured Section Created

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import HeroSection from "./components/HeroSection";
 import Aboutsection from "./components/Aboutsection";
 import TeacherSection from "./components/TeacherSection";
+import FeaturedSection from "./components/FeaturedSection";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-[60vh] w-screen">
       <HeroSection />
       <Aboutsection />
       <TeacherSection />
+      <FeaturedSection />
     </div>
   );
 };
---
+++ src/components/FeaturedSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageStart from "../images/brownstart.svg";
+import PageEnd from "../images/brownend.svg";
+
+const FeaturedSection = () => {
+  return (
+    <section>
+      <div className="h-[70vh] w-full bg-[#291217]"></div>
+    </section>
+  );
+};
+
+export default FeaturedSection;
