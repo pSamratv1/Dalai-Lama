3f32ade5615bafed442049bd8a8a15b152b293ee
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-27 23:49:59 +0545

    Icon Position Configuring

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -13,35 +13,30 @@ const FeaturedSection = () => {
   return (
     <>
       <img className="" src={PageStart} />
-      <section className="h-[60vh] grid gap-4 p-6 bg-[#291217]">
-        {/* Top 4 Images */}
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 justify-between">
-          <img
-            className="w-[50vw] mx-auto"
-            src={NewYorkTimes}
-            alt="The New York Times"
-          />
-          <img className="w-[60vw] mx-auto" src={Rachel} alt="Rachael" />
-          <img
-            className="w-[40vw] mx-auto"
-            src={GmAmerica}
-            alt="Good Morning America"
-          />
-          <img
-            className="w-[40vw] mx-auto"
-            src={WashingTon}
-            alt="The Washington Post"
-          />
-        </div>
-
-        {/* Lower 2 Images */}
-        <div className="grid grid-cols-2 gap-4">
-          <img className="w-32 mx-auto" src={TheView} alt="The View" />
-          <img
-            className="w-32 mx-auto"
-            src={Trevor}
-            alt="The Trevor Noah Show"
-          />
+      <section className="h-[60vh] py-20 bg-[#291217]">
+        <div className="px-6">
+          <div className="flex flex-col md:flex-row px-16 space-y-12">
+            <div className="flex  gap-4">
+              <img
+                className="h-[4vh] w-[35vw] mx-auto"
+                src={NewYorkTimes}
+                alt="The New York Times"
+              />
+              <img className="w-[30vw] mx-auto" src={Rachel} alt="Rachael" />
+            </div>
+            <div className="flex gap-4 items-center">
+              <img
+                className="w-[20vw] mx-auto"
+                src={GmAmerica}
+                alt="Good Morning America"
+              />
+              <img
+                className="w-[40vw] mx-auto"
+                src={WashingTon}
+                alt="The Washington Post"
+              />
+            </div>
+          </div>
         </div>
       </section>
 
@@ -51,3 +46,22 @@ const FeaturedSection = () => {
 };
 
 export default FeaturedSection;
+
+{
+  /* <div className="grid grid-cols-2 gap-4 md:grid-cols-4 justify-between">
+
+         
+          
+         
+        </div>
+
+
+        <div className="grid grid-cols-2 gap-4">
+          <img className="w-32 mx-auto" src={TheView} alt="The View" />
+          <img
+            className="w-32 mx-auto"
+            src={Trevor}
+            alt="The Trevor Noah Show"
+          />
+        </div> */
+}
