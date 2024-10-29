a74df7d94759bfa9cbe8947c18d119db19c656ed
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-29 15:51:13 +0545

    Testimonial Added

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -4,10 +4,15 @@ import RightArrow from "../images/RightArrow.svg";
 
 const Testimonial = () => {
   return (
-    <section className="testimonial h-[50vh] bg-red-400">
-      <div className=" p-24">
-        <div>Skeptics Speak</div>
-        <div>Having permission to sit for what ever </div>
+    <section className="testimonial h-[50vh] px-4">
+      <div className="flex flex-col justify-center py-14 space-y-10">
+        <div className="bg-red-400 text-center text-[16px] uppercase font-[600] tracking-[0.3rem]">
+          Skeptics Speak
+        </div>
+        <div className="flex-col text-center space-y-6 min-h-[20vh]">
+          <div> Having permission to sit for what ever </div>
+          <div>- Jessica</div>
+        </div>
         <div className="flex justify-center">
           <img src={LeftArrow} alt="" srcset="" />
           <img src={RightArrow} alt="" srcset="" />
