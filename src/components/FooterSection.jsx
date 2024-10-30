14d37fb8982fb19fed808471903ce2c78e5b8304
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-31 00:53:38 +0545

    All small device about to complete

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const FooterSection = () => {
   return (
     <section className="footer-section h-full">
-      <div className="bg-[#291217] h-full text-[#f4f3e9] py-14">
-        <div className="flex"></div>
+      <div className="bg-[#291217] h-full text-[#f4f3e9] px">
+        <div className="flex flex-col"></div>
       </div>
     </section>
   );
